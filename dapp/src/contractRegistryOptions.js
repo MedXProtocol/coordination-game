3fb7575a63e9c416858e94ba29d1e87ee96480e7
Charles Bergeron <chuck.bergeron@gmail.com>
2018-10-18 13:45:50 -0700

    * don't bother w/ PLCR Voting interface for the meantime, can pull it from sibling contracts

--- dapp/src/contractRegistryOptions.js
+++ dapp/src/contractRegistryOptions.js
@@ -1,7 +1,6 @@
 import coordinationGameConfig from '#/CoordinationGame.json'
 import tilRegistryConfig from '#/TILRegistry.json'
 import parameterizerConfig from '#/Parameterizer.json'
-import plcrVotingConfig from '#/PLCRVoting.json'
 import workConfig from '#/Work.json'
 import workTokenConfig from '#/WorkToken.json'
 
@@ -12,7 +11,6 @@ export const contractRegistryOptions = {
     CoordinationGame: abiFactory(coordinationGameConfig.abi),
     TILRegistry: abiFactory(tilRegistryConfig.abi),
     Parameterizer: abiFactory(parameterizerConfig.abi),
-    PLCRVoting: abiFactory(plcrVotingConfig.abi),
     Work: abiFactory(workConfig.abi),
     WorkToken: abiFactory(workTokenConfig.abi)
   }
--- dapp/src/sagas/addContractsSaga.js
+++ dapp/src/sagas/addContractsSaga.js
@@ -6,7 +6,6 @@ import { all } from 'redux-saga/effects'
 import coordinationGameConfig from '#/CoordinationGame.json'
 import tilRegistryConfig from '#/TILRegistry.json'
 import parameterizerConfig from '#/Parameterizer.json'
-import plcrVotingConfig from '#/PLCRVoting.json'
 import workConfig from '#/Work.json'
 import workTokenConfig from '#/WorkToken.json'
 
@@ -28,7 +27,6 @@ export const addContractsSaga = function* () {
   yield addTruffleArtifactAddresses(coordinationGameConfig)
   yield addTruffleArtifactAddresses(tilRegistryConfig)
   yield addTruffleArtifactAddresses(parameterizerConfig)
-  yield addTruffleArtifactAddresses(plcrVotingConfig)
   yield addTruffleArtifactAddresses(workConfig)
   yield addTruffleArtifactAddresses(workTokenConfig)
 }
