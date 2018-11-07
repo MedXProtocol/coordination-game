6c705874151c95a1c1154be018bd60e95d99cdab
Charles Bergeron <chuck.bergeron@gmail.com>
2018-11-07 12:01:14 -0800

    * privacy mode-ready!

--- dapp/src/components/LoginToMetaMask.jsx
+++ dapp/src/components/LoginToMetaMask.jsx
@@ -23,29 +23,36 @@ export const LoginToMetaMask =
 
         this.state = {
           enoughTimePassed: false,
-          modalState: false
+          modalState: false,
+          error: '',
+          isUnlocked: undefined,
+          isEnabled: undefined,
+          isApproved: undefined
         }
-      }
-
-      toggleModal = () => {
-        // this.setState((prev, props) => {
-        //   return { modalState: !prev.modalState }
-        // })
-      }
-
-      componentWillReceiveProps(nextProps) {
-        this.determineModalState(nextProps)
       }
 
       componentDidMount() {
         this.props.setTimeout(
           () => {
+            this.interval = this.props.setInterval(this.checkEthereum, 1000)
+            console.log('here')
+            console.log(this.interval)
+
+
             this.setState({
               enoughTimePassed: true
             }, this.determineModalState)
           },
           ACTIVATE_DURATION
         )
+      }
+
+      componentWillReceiveProps(nextProps) {
+        this.determineModalState(nextProps)
+      }
+
+      componentWillUmount () {
+        this.props.clearInterval(this.interval)
       }
 
       determineModalState(nextProps) {
@@ -59,6 +66,51 @@ export const LoginToMetaMask =
         this.setState({
           modalState: newModalState
         })
+      }
+
+      enableEthereum = async (e) => {
+        e.preventDefault()
+
+        this.setState({ error: '' })
+
+        if (window.ethereum) {
+          try {
+            await window.ethereum.enable()
+          } catch (error) {
+            if (error !== 'User rejected provider access') {
+              console.error(error)
+            }
+
+            this.setState({ error: error })
+          }
+        }
+      }
+
+      checkEthereum = async () => {
+        console.log('checking')
+        if (window.ethereum) {
+          let isUnlocked,
+            isEnabled,
+            isApproved
+
+          if (window.ethereum._metamask) {
+            isUnlocked = await window.ethereum._metamask.isUnlocked()
+            isEnabled  = await window.ethereum._metamask.isEnabled()
+            isApproved = await window.ethereum._metamask.isApproved()
+          }
+
+          this.setState({
+            isUnlocked,
+            isEnabled,
+            isApproved
+          })
+        }
+      }
+
+      toggleModal = () => {
+        // this.setState((prev, props) => {
+        //   return { modalState: !prev.modalState }
+        // })
       }
 
       render () {
@@ -75,9 +127,36 @@ export const LoginToMetaMask =
                 We see you're using MetaMask, nice!
               </h4>
 
-              <h6 className="is-size-6">
-                To continue click the fox in the top-right corner to log in to your MetaMask account.
-              </h6>
+              {this.state.isUnlocked && !this.state.isApproved
+                ? (
+                  <div className="text-center">
+                    <br />
+                    <a className="button is-light is-primary " onClick={this.enableEthereum}>
+                      Authorize The Coordination Game
+                    </a>
+                    <br />
+                    <br />
+
+                    {this.state.error ?
+                      (
+                        <p>
+                          There was an error: {this.state.error}
+                        </p>
+
+                      ) : null
+                    }
+                  </div>
+                ) : null
+              }
+
+              {!this.state.isUnlocked
+                ? (
+                  <h6 className="is-size-6">
+                    To continue click the fox in the top-right corner to log in to your MetaMask account.
+                  </h6>
+                )
+                : null
+              }
             </div>
           </Modal>
         )
