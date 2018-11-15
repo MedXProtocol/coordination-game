5fef04aa311499e7fbab1fa9bcb9092dcfba4cee
Brendan Asselstine <mail.asselstine@gmail.com>
2018-11-15 10:53:24 -0800

    Uncommented ropsten network config

--- truffle-config.js
+++ truffle-config.js
@@ -31,7 +31,7 @@
 // require('babel-register')
 // require('babel-polyfill')
 
-// var HDWalletProvider = require("truffle-hdwallet-provider")
+var HDWalletProvider = require("truffle-hdwallet-provider")
 
 module.exports = {
   /**
@@ -59,24 +59,24 @@ module.exports = {
       gas: 8000000,
       gasPrice: 2 * 1000000000
     },
-    // ropsten: {
-    //   provider: () => new HDWalletProvider(process.env.HDWALLET_MNEMONIC, process.env.REACT_APP_ROPSTEN_PROVIDER_URL),
-    //   network_id: 3,
-    //   gas: 8000000,
-    //   gasPrice: 92 * 1000000000
-    // },
-    // rinkeby: {
-    //   provider: () => new HDWalletProvider(process.env.HDWALLET_MNEMONIC, process.env.REACT_APP_RINKEBY_PROVIDER_URL),
-    //   network_id: 4,
-    //   gas: 4700000,
-    //   gasPrice: 60 * 1000000000
-    // },
-    // mainnet: {
-    //   provider: () => new HDWalletProvider(process.env.HDWALLET_MNEMONIC_MAINNET, process.env.REACT_APP_MAINNET_PROVIDER_URL),
-    //   network_id: 1,
-    //   gas: 4700000,
-    //   gasPrice: 2 * 1000000000
-    // }
+    ropsten: {
+      provider: () => new HDWalletProvider(process.env.HDWALLET_MNEMONIC, process.env.REACT_APP_ROPSTEN_PROVIDER_URL),
+      network_id: 3,
+      gas: 8000000,
+      gasPrice: 92 * 1000000000
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(process.env.HDWALLET_MNEMONIC, process.env.REACT_APP_RINKEBY_PROVIDER_URL),
+      network_id: 4,
+      gas: 4700000,
+      gasPrice: 60 * 1000000000
+    },
+    mainnet: {
+      provider: () => new HDWalletProvider(process.env.HDWALLET_MNEMONIC_MAINNET, process.env.REACT_APP_MAINNET_PROVIDER_URL),
+      network_id: 1,
+      gas: 4700000,
+      gasPrice: 2 * 1000000000
+    }
   },
 
   // Set default mocha options here, use special reporters etc.
