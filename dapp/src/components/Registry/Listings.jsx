cd4c664552192a0fb4dc7d8a904308a6f2335f37
Charles Bergeron <chuck.bergeron@gmail.com>
2018-12-20 15:27:41 -0800

    * copy fix

--- dapp/src/components/Registry/Listings.jsx
+++ dapp/src/components/Registry/Listings.jsx
@@ -98,7 +98,7 @@ export const Listings = connect(mapStateToProps)(
             <div className="blank-state">
               <div className="blank-state--inner has-text-grey-lighter">
                 <span className="is-size-6">
-                  You have no listings yet -&nbsp;
+                  Currently no listings in the registry -&nbsp;
                   <Link to={routes.REGISTER_TOKEN}>
                     Register a Token Now
                   </Link>
